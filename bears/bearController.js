f9b4c2f0de197bb942bb474074518bad3cf42746
RiaClay <sherria.rydyn@gmail.com>
2018-04-09 13:09:40 -0500

    created bearModel.js. Connected to Mongoose. Connected to Mongo. Completed examples from lecture.

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,17 +1,46 @@
 const router = require('express').Router();
 
+const Bear = require('./bearModel');
+
 router
   .route('/')
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+
+  Bear
+    .find({})
+    .then(bears => {    //to limit the number of query results set limit Bear.find({}).limit(20).then(bears => {})
+    res.status(200).json(bears);
+  })
+    .catch(err => {
+      res.status(500).json(err);
+    })
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    const bear = new Bear(req.body);
+  
+  bear
+    .save()
+    .then(savedBear => {
+      res.status(201).json(savedBear);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+    
   });
 
 router
   .route('/:id')
   .get((req, res) => {
+
+    Bear
+      .findById(req.params.id)
+      .then(bears => {
+        res.status(200).json(bears);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      })
     res.status(200).json({ route: '/api/bears/' + req.params.id });
   })
   .delete((req, res) => {
